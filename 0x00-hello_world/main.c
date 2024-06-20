e1ed697d15c7947f2a3260d0ff492ddb8cd60878
NO-MM <khuzwanohnoh@gmail.com>
2024-06-20 14:11:38 -0700

    Main.c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -Entry point, prints Hello, World
+ *main - Entry point, printts Hello, World
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("Hello, World");
-  return (0);
+printf("Hello, World"); /*Inline Comment*/
+return (0);
 }
