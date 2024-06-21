a8d97c325b7fe001109e35b162e777b997b0545c
NO-MM <khuzwanohnoh@gmail.com>
2024-06-21 05:30:28 -0700

    Main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - Enty point, prints "Programming is like building a multiligual puzzle
+ *main - Enty point, prints "Programming is like building a multilingual puzzle
  *
  *return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multiligual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
