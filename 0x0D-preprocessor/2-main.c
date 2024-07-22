1b3cad63463dac5df881ea79463e31c550010baf
NO-MM <khuzwanohnoh@gmail.com>
2024-07-22 04:22:35 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Programme that prints the name of the file.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf((current file)) "%s\n", _FILE_;
+return (0);
+}
