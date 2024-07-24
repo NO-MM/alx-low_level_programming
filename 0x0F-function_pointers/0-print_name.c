bbcad4550a2ee5614ab20ec7077d3f32db5e0c03
NO-MM <khuzwanohnoh@gmail.com>
2024-07-24 12:58:45 -0700

    Function-pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - This function prints a name.
+ * 
+ * @f: a variable to hold a string name.
+ * @name: name of the person.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,20 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+* op_mod - Return the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a and b.
+ */
+int (*get_op_func(char *s))(int, int)
+}
+int i = 0;
+while (ops[i].op != NULL)
+{
+if (*(ops[i].op == *s && *(S + 1) == '\0')
+return (ops[i].f);
+i++;
+}
+return (NULL);
+}
