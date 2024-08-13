72d385b06f0b0502f5a5c01999771d7b833e46ce
NO-MM <khuzwanohnoh@gmail.com>
2024-08-12 20:36:27 -0700

    updates

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,12 @@ int create_file(const char *filename, char *text_content)
 int fd;
 int nl;
 int rwr;
-if(!filename)
+if (!filename)
 return (-1);
 fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if(fd == -1)
+if (fd == -1)
 return (-1);
-if(text_content == NULL)
+if (text_content == NULL)
 text_content = " ";
 for (nl = 0; text_content[nl]; nl++)
 ;
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 int fd;
 int nl;
 int rwr;
-if(!filename)
+if (!filename)
 return (-1);
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
@@ -20,10 +20,10 @@ return (-1);
 if (text_content)
 {
 for (nl = 0; text_content[nl]; nl++)
-rwr = write (fd, text_content, nl);
+rwr = write(fd, text_content, nl);
 if (rwr == -1)
 return (-1);
 }
 close(fd);
-return(1);
+return (1);
 }
