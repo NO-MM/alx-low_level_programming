e0aef107bf08d1ca085a2c88cb757945f78d3b28
NO-MM <khuzwanohnoh@gmail.com>
2024-08-13 01:13:12 -0700

    updates cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,20 +4,20 @@
 /**
  * file_from_to - A function that checks if a file is opened
  * and handle errors.
- * @file_f: A file to copy from to chech.
- * @file_t: A destination to copy a file.
+ * @file_from: A file to copy from to chech.
+ * @file_to: A destination to copy a file.
  * @argv: argument vector.
  *
  * Return: No return.
  */
-void file_from_to(int file_f, int file_t, char *argv[])
+void file_from_to(int file_from, int file_to, char *argv[])
 {
-if (file_f == -1)
+if (file_from == -1)
 {
 dprintf(STDERR_FILENO, "Erro: Cant't read from file %s\n", argv[1]);
 exit(98);
 }
-if (file_t == -1)
+if (file_to == -1)
 {
 dprintf(STDERR_FILENO, "Erro: Cant't write to file %s\n", argv[2]);
 exit(99);
@@ -32,7 +32,7 @@ exit(99);
  */
 int main(int argc, char *argv[])
 {
-int file_f, file_t, err_close;
+int file_from, file_to, err_close;
 ssize_t num_chars, nwr;
 char buffer[1024];
 if (argc != 3)
@@ -40,29 +40,29 @@ if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-file_f = open(argv[1], O_RDONLY);
-file_t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-file_from_to(file_f, file_t, argv);
+file_from = open(argv[1], O_RDONLY);
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+file_from_to(file_from, file_to, argv);
 num_chars = 1024;
 while (num_chars == 1024)
 {
-num_chars = read(file_f, buffer, 1024);
+num_chars = read(file_from, buffer, 1024);
 if (num_chars == -1)
 file_from_to(-1, 0, argv);
-nwr = write(file_t, buffer, num_chars);
+nwr = write(file_to, buffer, num_chars);
 if (nwr == -1)
 file_from_to(0, -1, argv);
 }
-err_close = close(file_f);
+err_close = close(file_from);
 if (err_close == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_f);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 exit(100);
 }
-err_close = close(file_t);
+err_close = close(file_to);
 if (err_close == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close ft %d\n", file_t);
+dprintf(STDERR_FILENO, "Error: Can't close ft %d\n", file_to);
 exit(100);
 }
 return (0);
