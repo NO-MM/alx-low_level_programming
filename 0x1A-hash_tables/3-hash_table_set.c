d258cc1b6f3521ca783cd2e94e61b517290ad30e
NO-MM <khuzwanohnoh@gmail.com>
2024-10-11 20:57:18 -0700

    tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,37 @@
 #include "hash_tables.h"
 
 /**
+ * make_hash_node - A function that creates a new hash node.
+ * @key: A key for the node.
+ * @value: A value for the node.
+ *
+ * Return: the  new node, or NULL on failure.
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+    hash_node_t *node;
+
+    node = malloc(sizeof(hash_node_t));
+    if (node == NULL)
+            return (NULL);
+    node->key = strdup(key);
+    if (node->key == NULL)
+    {
+            free(node);
+            return (NULL);
+    }
+    node->value = strdup(value);
+    if (node->value == NULL)
+    {
+      free(node->key);
+      free(node);
+      return (NULL);
+    }
+    node->next = NULL;
+    return (node);
+}
+      
+/**
  * hash_table_set - A function that sets a key to a value in the hash table.
  * @ht: A hash to add element.
  * @key: A key for  the data.
